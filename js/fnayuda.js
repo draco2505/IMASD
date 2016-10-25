116c92146b6394a3e729887abef75608832511b5
draco2505 <draco02@live.com.mx>
2016-10-25 18:01:49 -0500

    Add files via upload

---
+++ js/fnayuda.js
@@ -0,0 +1,9 @@
+﻿// JScript File
+
+function MostrarAyuda(strId){
+    tempid=document.getElementById(strId);
+    if(tempid.style.visibility=='visible')
+        tempid.style.visibility='hidden';
+    else
+        tempid.style.visibility='visible';
+}

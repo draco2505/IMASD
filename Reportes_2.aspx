7ff14fd238fe469854fea2a93477f6223e2058c8
draco2505 <draco02@live.com.mx>
2016-11-24 00:39:36 -0600

    cambio contra

---
+++ Reportes_2.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/mpInterna.master" AutoEventWireup="false" CodeFile="Reportes_2.aspx.vb" Inherits="Reportes_2" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphCabecera" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphPrincipalInterna" Runat="Server">
+    <%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, 
+      PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+    <form id="formulario1" action="/" method="post" runat="server" >
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server"></rsweb:ReportViewer>
+    </form>
+</asp:Content>
+
---
+++ Reportes_2.aspx.vb
@@ -0,0 +1,44 @@
+﻿Imports Microsoft.Reporting.WebForms
+Imports System.Configuration
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Data
+Imports System.Data.SqlClient
+
+
+Partial Class Reportes_2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            If (Session(System.Web.Configuration.WebConfigurationManager.AppSettings("SesionCampoUsuario").ToString) IsNot Nothing) Then
+                Select Case CInt(Session(System.Web.Configuration.WebConfigurationManager.AppSettings("SesionCampoNivel").ToString))
+                    Case clsAuthentication.AuthorizationLevelList.Administering
+                        Exit Select
+                    Case clsAuthentication.AuthorizationLevelList.Financial
+                        Server.Transfer("~/Inicio.aspx", False)
+                    Case clsAuthentication.AuthorizationLevelList.LimitedUpdating
+                        Server.Transfer("~/Inicio.aspx", False)
+                    Case clsAuthentication.AuthorizationLevelList.Consulting
+                        Server.Transfer("~/Inicio.aspx", False)
+                    Case Else
+                        Server.Transfer("~/Inicio.aspx", False)
+                End Select
+            Else
+                Server.Transfer("~/Inicio.aspx", False)
+            End If
+        End If
+
+        
+    End Sub
+
+    Private Sub BindReport(ByVal indexreport As Integer)
+
+
+    End Sub
+
+End Class
